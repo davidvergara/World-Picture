ea43b7cc07e42b19cbb75e45dc8221d62bf0ef76
davidvergara <david13z@hotmail.com>
2015-12-02 19:19:17 +0100

    Upload image

---
+++ database/migrations/2015_12_02_175155_create_fileentries_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFileentriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fileentries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('filename');
+            $table->string('path');
+            $table->decimal('lattitude', 6, 6);
+            $table->decimal('longitude', 6, 6);
+
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('fileentries');
+
+    }
+}
